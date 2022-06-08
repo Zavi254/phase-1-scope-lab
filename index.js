3d457872e416456ed7781e780678e199383745de
Steven-Otieno <steveotianga8991@gmail.com>
2022-06-08 15:05:59 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'Steve';
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer(){
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Steve";
+}
+ 
